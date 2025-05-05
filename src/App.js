d41630f534092efecac25d1b7b99ac7dc3d64773
seojiho <happyluck0426@gmail.com>
2025-05-05 17:08:31 +0900

    css:페이지 레이아웃 구현

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import styles from "./App.module.css"
 
 function App() {
-  return <div className={styles}></div>
+  return (
+    <>
+      <div className={styles.nav}>nav</div>
+      <div className={styles.header}>header</div>
+      <div className={styles.listContainer}>listContainer</div>
+      <div className={styles.footer}>footer</div>
+    </>
+  )
 }
 
 export default App
