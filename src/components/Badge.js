1f56837dd3fd953ae12faad8914d4d31a90717ff
seojiho <happyluck0426@gmail.com>
2025-05-06 00:57:56 +0900

    component: 뱃지 컴포넌트 생성

---
+++ src/components/Badge.js
@@ -0,0 +1,6 @@
+import styles from "./Badge.module.css"
+import cx from "clsx"
+
+export default function Badge({ title, color }) {
+  return <div className={cx(styles.badge, styles[color])}>{title}</div>
+}
