aea01b138d4b66f140a03e303c43f9192eb144da
seojiho <happyluck0426@gmail.com>
2025-05-05 23:18:50 +0900

    component:버튼 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import styles from "./App.module.css"
+import Header from "./Header"
+import Button from "./components/Button"
 
 function App() {
   return (
     <>
       <div className={styles.nav}>nav</div>
-      <div className={styles.header}>header</div>
-      <div className={styles.listContainer}>listContainer</div>
+      <Header />
+      <div className={styles.listContainer}>
+        <Button
+          style={{
+            fontSize: "14px",
+            backgroundColor: "green",
+            color: "white",
+          }}
+        >
+          New Issue
+        </Button>
+      </div>
       <div className={styles.footer}>footer</div>
     </>
   )
