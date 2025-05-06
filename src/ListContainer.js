0c277824baa77a8b87818d67d627ac21dc1921c8
seojiho <happyluck0426@gmail.com>
2025-05-07 01:28:20 +0900

    component: 페이지네이션 컴포넌트 생성

--- src/ListContainer.js
+++ src/ListContainer.js
@@ -4,52 +4,63 @@ import Button from "./components/Button"
 import ListItem from "./components/ListItem"
 import ListItemLayout from "./components/ListItemLayout"
 import Modal from "./components/Modal"
+import Pageination from "./components/Pageination"
 import cx from "clsx"
 
 export default function ListContainer() {
   const [inputValue, setInputValue] = useState("is:pr is:open")
   const [list, setList] = useState([])
+  const [page, setPage] = useState(1)
 
   useEffect(() => {
     console.log({ inputValue })
   }, [inputValue])
 
   return (
-    <div className={styles.listContainer}>
-      <div className={styles.topSection}>
-        <input
-          className={styles.input}
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-        />
-        <Button
-          style={{
-            fontSize: "14px",
-            backgroundColor: "green",
-            color: "white",
-          }}
-        >
-          New Issue
-        </Button>
-      </div>
-      <OpenClosedFilters />
-      <ListItemLayout className={styles.listFilter}>
-        <ListFilter onChangeFilter={(filteredData) => {}} />
-      </ListItemLayout>
-      <div className={styles.container}>
-        {list.map((ListItem, index) => (
-          <ListItem
-            key={index}
-            badges={[
-              {
-                color: "red",
-                title: "Bug",
-              },
-            ]}
+    <>
+      <div className={styles.listContainer}>
+        <div className={styles.topSection}>
+          <input
+            className={styles.input}
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
           />
-        ))}
+          <Button
+            style={{
+              fontSize: "14px",
+              backgroundColor: "green",
+              color: "white",
+            }}
+          >
+            New Issue
+          </Button>
+        </div>
+        <OpenClosedFilters />
+        <ListItemLayout className={styles.listFilter}>
+          <ListFilter onChangeFilter={(filteredData) => {}} />
+        </ListItemLayout>
+        <div className={styles.container}>
+          {list.map((listItem, index) => (
+            <ListItem
+              key={index}
+              badges={[
+                {
+                  color: "red",
+                  title: "Bug",
+                },
+              ]}
+            />
+          ))}
+        </div>
       </div>
-    </div>
+      <div className={styles.PageinationContainer}>
+        <Pageination
+          maxPage={10}
+          currentPage={page}
+          onClickPageButton={(number) => setPage(number)}
+        />
+      </div>
+    </>
   )
 }
 
