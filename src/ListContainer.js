d415a4fb4c7202adbb27b43623cdfa5f8e85a19a
seojiho <happyluck0426@gmail.com>
2025-05-11 15:43:29 +0900

    feat: useSearchParams로 페이지네이션 상태 URL과 연동

--- src/ListContainer.js
+++ src/ListContainer.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import { useSearchParams } from "react-router-dom"
 import axios from "axios"
 import styles from "./ListContainer.module.css"
 import Button from "./components/Button"
@@ -13,10 +14,12 @@ export default function ListContainer() {
   const [inputValue, setInputValue] = useState("is:pr is:open")
   const [checked, setChecked] = useState(false)
   const [list, setList] = useState([])
-  const [page, setPage] = useState(1)
   const [isOpenMode, setIsOpenMode] = useState(true)
   const [params, setParams] = useState()
   const maxPage = 10
+
+  const [searchParams, setSearchParams] = useSearchParams()
+  const page = parseInt(searchParams.get("page"), 10)
 
   async function getData(params) {
     const data = await axios.get(`${GITHUB_API}/repos/facebook/react/issues`, {
@@ -74,7 +77,9 @@ export default function ListContainer() {
         <Pageination
           maxPage={maxPage}
           currentPage={page}
-          onClickPageButton={(number) => setPage(number)}
+          onClickPageButton={(PageNumber) =>
+            setSearchParams({ page: PageNumber })
+          }
         />
       </div>
     </>
