d1ab38d4f006ebb7da289030cf8ea3845ad10f4a
seojiho <happyluck0426@gmail.com>
2025-05-11 16:30:28 +0900

    feat: CreateIssue 폼에 타이틀 유효성 검사 및 포커스 처리 추가

--- src/pages/CreateIssuse.js
+++ src/pages/CreateIssuse.js
@@ -1,31 +1,48 @@
+import { useRef } from "react"
 import styles from "./CreateIssuse.module.css"
 import Button from "../components/Button"
 import cx from "clsx"
 
 export default function CreateIssuse() {
+  const ref = useRef()
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    if (e.target.elements.title.value === "") {
+      alert("타이틀을 입력해주세요")
+      ref.current.focus()
+    }
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.avatar}></div>
       <div className={cx(styles.inputWrapper, styles.border)}>
-        <input
-          className={cx(styles.input, styles.border)}
-          placeholder="Title"
-        ></input>
-        <textarea
-          className={cx(styles.input, styles.textarea, styles.border)}
-          placeholder="Leave a comment"
-        ></textarea>
-        <div className={styles.buttonWrapper}>
-          <Button
-            style={{
-              fontSize: "14px",
-              backgroundColor: "green",
-              color: "white",
-            }}
-          >
-            New Issue
-          </Button>
-        </div>
+        <form onSubmit={handleSubmit}>
+          <input
+            name="title"
+            ref={ref}
+            className={cx(styles.input, styles.border)}
+            placeholder="Title"
+          ></input>
+          <textarea
+            name="body"
+            className={cx(styles.input, styles.textarea, styles.border)}
+            placeholder="Leave a comment"
+          ></textarea>
+          <div className={styles.buttonWrapper}>
+            <Button
+              type="submit"
+              style={{
+                fontSize: "14px",
+                backgroundColor: "green",
+                color: "white",
+              }}
+            >
+              New Issue
+            </Button>
+          </div>
+        </form>
       </div>
     </div>
   )
